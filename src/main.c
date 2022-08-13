2ed525e1fc0e70939bb2a784925299e6edad27b5
Stefano Fontana <stefano.fontana.2000@gmail.com>
2022-08-13 20:53:37 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -36,26 +36,27 @@ int main(void) {
     for (;;) {
         usr_btn_task(&rst_button);
         if (USB_DeviceState == DEVICE_STATE_Configured) {
-            if (LineEncoding.BaudRateBPS == 1200 && !init) {
-                if (gdb_state_g.state != GDB_STATE_SIGHUP) { //dw was successfully connected
-                    if (debug_wire_halt())
-                        dw_cmd(DW_CMD_DISABLE); //disable dw until power cycle
-                }
-                gdb_deinit();
-                _delay_ms(100);
-                stk500_init();
-                init = 1;
-            } else if (LineEncoding.BaudRateBPS != 1200 && init) {
-                stk500_deinit();
-                _delay_ms(100);
-                gdb_init();
-                init = 0;
-            }
+            gdb_task();
+//            if (LineEncoding.BaudRateBPS == 1200 && !init) {
+//                if (gdb_state_g.state != GDB_STATE_SIGHUP) { //dw was successfully connected
+//                    if (debug_wire_halt())
+//                        dw_cmd(DW_CMD_DISABLE); //disable dw until power cycle
+//                }
+//                gdb_deinit();
+//                _delay_ms(100);
+//                stk500_init();
+//                init = 1;
+//            } else if (LineEncoding.BaudRateBPS != 1200 && init) {
+//                stk500_deinit();
+//                _delay_ms(100);
+//                gdb_init();
+//                init = 0;
+//            }
         }
 
         //I don't think this is good...
-        if(!init) gdb_task();
-        else stk500_task();
+//        if(!init) gdb_task();
+//        else stk500_task();
 
         USB_USBTask();
     }
