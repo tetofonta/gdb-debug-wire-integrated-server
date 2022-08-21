236f51ef4f4b422e7abe0a1086af06ad43e5404b
Stefano Fontana <stefano.fontana.2000@gmail.com>
2022-08-21 18:35:41 +0200

    rtt documentation

--- avrtest/includes/rtt.h
+++ avrtest/includes/rtt.h
@@ -5,6 +5,7 @@
 #ifdef BUILD_TYPE
 #if BUILD_TYPE == Debug
 
+
 #ifndef __DEBUG_BUFFER_SIZE__
     #define __DEBUG_BUFFER_SIZE__ 64
 #else
@@ -13,9 +14,19 @@
     #endif
 #endif
 
+/**
+ * This call performs a rtt tranfer if rtt has been enabled from the debugger.
+ * It will set the message size (limiting to how much space is available) and the available flag
+ * then it will break waiting for the debugger to resume.
+ *
+ * burden of the debugger is to reset the flags and continue execution.
+ */
 void rtt_log(const char *buffer, uint8_t size);
+
+/**
+ * Initializes data structures for rtt operations.
+ */
 void rtt_init(void);
-void rtt_enable(void);
 
 #else
 
